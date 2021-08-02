0481c4a956502601bb34d155c15d55043de37ca8
Samantha Defays <samantha.defays@gmail.com>
2021-08-02 22:20:28 +0200

    Début de présentation et ébauche des projets à présenter

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
     CommonModule,
     ProfileRoutingModule
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  public projects = [
+    {
+      titre: 'Portfolio 1.0',
+      type: 'Présentation de projets',
+      legende: '',
+      description: '',
+      technos: ['HTML', 'CSS', 'JavaScript', 'PHP'],
+      date: '09/02/2021'
+    },
+    {
+      titre: 'Kraken Play',
+      type: 'E-Commerce',
+      legende: 'Projet de diplôme de formation',
+      description: '',
+      technos: ['HTML', 'CSS', 'JavaScript', 'Ajax', 'PHP', 'Framework perso', 'MySQL'],
+      date: '03/08/2020'
+    },
+    {
+      titre: 'Restaurant',
+      type: '',
+      legende: '',
+      description: '',
+      technos: ['HTML', 'CSS', 'JavaScript', 'Ajax', 'PHP', 'Framework 3WA', 'MySQL'],
+      date: '21/03/2020'
+    },
+    {
+      titre: 'Blog',
+      type: '',
+      legende: '',
+      description: '',
+      technos: ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL'],
+      date: '28/02/2020'
+    },
+    {
+      titre: 'Velib',
+      type: '',
+      legende: '',
+      description: '',
+      technos: ['HTML', 'CSS', 'JavaScript', 'Ajax', 'PHP'],
+      date: '22/02/2020'
+    },
+    {
+      titre: 'Painter',
+      type: 'Peinture digitale',
+      legende: '',
+      description: '',
+      technos: ['HTML', 'CSS', 'JavaScript', 'Canva'],
+      date: '05/02/2020'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
